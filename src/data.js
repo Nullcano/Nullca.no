80eaf9f9968b12fe528a6c39ccfdd9cb1b640512
Nullcano <mo@shi.moe>
2022-02-21 17:09:58 +0100

    yeayea

--- src/data.js
+++ src/data.js
@@ -5,7 +5,8 @@ export const posts = [
     description: 'Plotz is the classic free HTML5 modeller for Minecraft.',
     date: '2011-05-01',
     category: 'Minecraft',
-    tags: ['models', 'voxel', 'shapes', '3d'],
+    type: 'Website',
+    tags: ['Website', 'Editor', 'Tool'],
     content: `
       <p><a href="https://plotz.co.uk" target="_blank">Plotz</a> is the classic free HTML5 modeller for Minecraft. Generate spheres, ellipsoids, torus and more in your web browser.</p>
     `
@@ -15,7 +16,8 @@ export const posts = [
     description: 'Optifine is a mod for Minecraft that improves the performance of Minecraft.',
     date: '2012-01-01',
     category: 'Minecraft',
-    tags: ['mods', 'optimization', 'performance', 'minecraft'],
+    type: 'Mod',
+    tags: ['Website', 'Editor', 'Mod'],
     content: `
       <p><a href="https://optifine.net" target="_blank">Optifine</a> is a mod for Minecraft that improves the performance of Minecraft by improving the graphics and performance of the game.</p>
     `
@@ -25,7 +27,8 @@ export const posts = [
     description: 'NameMC lets you check the availability of Minecraft names and look up player skins.',
     date: '2015-02-01',
     category: 'Minecraft',
-    tags: ['mods', 'optimization', 'performance', 'minecraft'],
+    type: 'Website',
+    tags: ['Website', 'Tool'],
     content: `
       <p><a href="https://namemc.com" target="_blank">NameMC</a> lets you check the availability of Minecraft names, look up the name history of Minecraft accounts, view Minecraft skins in 3D, convert UUIDs, and much more.</p>
     `
@@ -35,7 +38,7 @@ export const posts = [
     description: 'Vanilla Tweaks help you tweak parts of vanilla Minecraft through resource packs and more.',
     date: '2018-05-01',
     category: 'Minecraft',
-    tags: ['vanilla', 'tweaks', 'vanillatweaks', 'vanilla tweaks', 'minecraft', 'texture pack', 'minecraft 1.14', 'vanilla tweaks 1.13', 'vanilla tweaks 1.14', 'vanilla tweaks 1.15', 'vanilla tweaks 1.16', 'vanilla tweaks download', 'vanilla tweaks minecraft download', 'vanilla tweaks minecraft 1.12 download', 'vanilla tweaks minecraft', 'vanilla tweaks 1.14', 'vanilla tweaks 1.12 download', 'vanilla tweaks 1.12.1', 'minecraft vanilla tweaks 1.13'],
+    tags: ['Website', 'Editor', 'Mod'],
     content: `
       <p><a href="https://vanillatweaks.net" target="_blank">Vanilla Tweaks</a> help you tweak parts of vanilla Minecraft through resource packs, data packs, and crafting tweaks.</p>
     `
@@ -45,7 +48,7 @@ export const posts = [
     description: 'Block Palettes is an online tool for Minecraft that lets you create custom palettes with blocks.',
     date: '2020-10-01',
     category: 'Minecraft',
-    tags: ['block', 'palette', 'editor'],
+    tags: ['Website', 'Editor', 'Tool'],
     content: `
       <p><a href="https://www.blockpalettes.com" target="_blank">Block Palettes</a> lets you find eye pleasing palettes to build with as well as create a place to connect with submitting your own palettes and monthly building contest!</p>
     `
@@ -55,19 +58,19 @@ export const posts = [
     description: 'Minecraft Tools is a collection of online tools for Minecraft.',
     date: '2015-02-01',
     category: 'Minecraft',
-    tags: [''],
+    tags: ['Website', 'Editor', 'Tool'],
     content: `
       <p><a href="https://minecraft.tools" target="_blank">Minecraft Tools</a> provides some online tools for Minecraft players like crafting list, flat world generator, enchant calculator, colored armor crafting, firework crafting, etc.</p>
     `
   }, {
     slug: 'digminecraft',
     title: 'DigMinecraft',
-    description: 'DigMinecraft is a Minecraft resource for finding and crafting Minecraft recipes and more.',
+    description: 'DigMinecraft is a Minecraft resource for finding crafting recipes and more.',
     date: '2015-02-01',
     category: 'Minecraft',
-    tags: ['digminecraft', 'minecraft', 'crafting recipes', 'mobs', 'game commands'],
+    tags: ['Website', 'Tool'],
     content: `
-      <p><a href="https://digminecraft.com" target="_blank">DigMinecraft</a> is a Minecraft resource for finding and crafting Minecraft recipes, mobs, and game commands.</p>
+      <p><a href="https://digminecraft.com" target="_blank">DigMinecraft</a> is a Minecraft resource for finding crafting recipes, mobs, and game commands.</p>
     `
   }, {
     slug: 'minecraft-skin-stealer',
@@ -75,7 +78,7 @@ export const posts = [
     description: 'Minecraft Skin Stealer is an online tool for Minecraft that lets you steal skins from other players.',
     date: '2013-01-01',
     category: 'Minecraft',
-    tags: [],
+    tags: ['Website', 'Tool'],
     content: `
       <p><a href="https://minecraftskinstealer.com" target="_blank">Minecraft Skin Stealer</a> lets you view and steal any minecraft skin with this easy online skin viewer and skin stealer tool.</p>
     `
@@ -85,7 +88,7 @@ export const posts = [
     description: 'Forge Mod Loader is a mod loader for Minecraft that lets you load certain mods in Minecraft.',
     date: '2015-02-01',
     category: 'Minecraft',
-    tags: [],
+    tags: ['Website', 'Mod'],
     content: `
       <p><a href="https://files.minecraftforge.net/net/minecraftforge/forge" target="_blank">Forge Mod Loader</a> is a loader for Minecraft mods. It was recently merged into Minecraft Forge, and is no longer developed on its own. FML was compatible with Risugami's ModLoader mods until 1.6, when ModLoader was discontinued. FML is itself a cleanroom rewrite of Risugami's ModLoader. Additionally, FML provides the mod list button added to the main screen when Forge is installed. It allows mods to use ASM to further change vanilla code, and also provides the Argo and Guava libraries. It also provides handlers for some features in a similar way to Forge, such as crafting and chat handlers.</p>
     `
