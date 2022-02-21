8a7908e677570e84ab48b2e3b8063db3c6804c1b
Nullcano <mo@shi.moe>
2022-02-21 14:46:11 +0100

    lore
    deep

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,12 +9,12 @@
   <meta property="og:title" content="Frontpage - Nullcano">
   <meta property="og:site_name" content="Nullcano">
   <meta property="og:url" content="https://nullca.no">
-  <meta property="og:description" content="Nullcano is a personally curated, but comprehensive archive of media and information about video games, and the industry surrounding it, while preserving its history.">
+  <meta property="og:description" content="Nullcano is a personally curated video game archive that is always growing. The goal is to index all types of media, files and articles about video games, the community surrounding it and make it easily accessible while also preserving its history.">
   <meta property="og:type" content="website">
   <meta property="og:image" content="">
 </svelte:head>
 
-<PageTitle title="Nullcano" description="Welcome to this personally curated video game archive that I am constantly growing. I sincerely hope you will find something useful. My goal is to index as much content and information about everything in the realm of video games and make them easily accessible with my global search filter." />
+<PageTitle title="Nullcano" description="This is a personally curated video game archive that is always growing. The goal is to index all types of media, files and articles about video games, the community surrounding it and make it easily accessible while also preserving its history." />
 
 <PageSubtitle title="Recent posts" />
 
