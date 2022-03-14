ec1e858e63df0a7aee7bb0508229d14e3f8a9b92
Nullcano <mo@shi.moe>
2022-03-14 02:13:19 +0100

    o
    k

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
 		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
 			split: false
 		})
 	}
-};
-
-export default config;
+}
