9bbef507bff76729d05812ec905ac36972199604
Nullcano <mo@shi.moe>
2022-05-29 22:08:50 +0200

    fix

--- src/lib/Journal/FeedRecent.svelte
+++ src/lib/Journal/FeedRecent.svelte
@@ -2,7 +2,7 @@
 	import { data } from '../../data';
 	import ItemsGrid from '$lib/ItemsGrid.svelte';
 	import PostCard from '$lib/PostCard.svelte';
-	let entries = data.filter((entry) => entry.type === 'journal').sort((a, b) => b.date - a.date);
+	let entries = data.filter((entry) => entry.type === 'journal').sort((a, b) => b.date - a.date).slice(0, 4);
 </script>
 
 <ItemsGrid>
