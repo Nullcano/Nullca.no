7f61bc51c0f64a550dc39db10c265ce7dd40de01
Nullcano <woldsteffen@gmail.com>
2022-05-30 14:27:00 +0200

    fix title for chrome

--- src/data.js
+++ src/data.js
@@ -17,7 +17,7 @@ export const data = [
     title: 'Volunta Case Study',
     date: '2020-04-28',
     content: [
-      `Me and a team partner from the academy has be working on an application called Volunta.`,
+      `Me and a team partner from the academy has been working on an application called Volunta.`,
       `Volunta is an organizing and publishing application for individuals and organizations with a common goal to offer or request services or tasks voluntarily. Core features include organizing community events, requesting help in form of work-power or equipment and joining in on topics of interest.`,
       `The application is currently in development and we are working on the frontend and backend.`,
       `The application is built with Blazor WebAssembly and MongoDB.`,
