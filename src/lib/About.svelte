8a287bc6fa44c98928d6ceba6c0db2524de95087
Nullcano <mo@shi.moe>
2022-09-07 22:30:38 +0200

    idk

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,4 +1,3 @@
-
 <section>
 	<div class="about">
 		<div class="left">
@@ -12,21 +11,17 @@
 			</div>
 		</div>
 		<div class="info">
-			<div class="outer-title">
-				<div class="inner-title">
-					<div class="title">
-						<div class="content featured">
-							<svg viewBox="0 0 44 32">
-								<polygon points="2,2 42,2 22,30" />
-								SVG:404
-							</svg>
-							<div class="dob">NO-211192</div>
-							<div class="name">
-								<span>Steffen</span>
-								<span>Wold</span>
-							</div>	
-						</div>
-					</div>
+			<div class="title items-end content-end featured">
+				<div class="name">
+					<span>Steffen</span>
+					<span>Wold</span>
+				</div>	
+				<div class="flex items-center gap-3">
+					<svg viewBox="0 0 44 32">
+						<polygon points="2,2 42,2 22,30" />
+						SVG:404
+					</svg>
+					<div class="dob">NO-211192</div>
 				</div>
 			</div>
 			<div class="divider"></div>
@@ -35,7 +30,7 @@
 					Based in Innlandet, Norway. Passionate about technology, user interfaces, and modern web applications.
 					Inspired by efficiency and productivity. Currently on internship at <a href="https://ikomm.no" target="_blank">Ikomm</a>.
 				</p>
-			</div>
+			</div>	
 		</div>
 	</div>
 	<div class="filler"></div>
@@ -43,8 +38,10 @@
 
 <style>
 	section {
+		height: 100vh;
 		display: grid;
 		grid-template-columns: 1fr 5rem;
+		overflow: hidden;
 	}
 	.about {
 		position: relative;
@@ -52,6 +49,8 @@
 		height: calc(100vh - 15rem);
 		display: grid;
 		grid-template-columns: 1fr 1fr;
+		overflow-y: auto;
+		overflow-x: hidden;
 	}
 	.left {
 		padding: 5rem;
@@ -92,39 +91,24 @@
 		justify-content: flex-end;
 		gap: 2rem;
 	}
-	.outer-title {
-		display: table;
-		vertical-align: bottom;
-	}
-	.inner-title {
-		padding: 50% 0;
-		height: 0;
-	}
 	.title {
-		margin-top: -50%;
-		display: block;
-		transform-origin: top left;
-		transform: rotate(-90deg) translate(-100%);
-		white-space: nowrap;
-	}
-	.title .content {
+		margin-top: -5rem;
 		display: flex;
-		flex-direction: column;
-		gap: 1rem;
+		gap: 2rem;
 	}
 	svg {
-		width: 1.5rem;
+		width: 1.5vw;
 		stroke: currentColor;
 		stroke-width: 4;
 		fill: none;
 	}
 	.dob {
-		font-size: 1.5rem;
+		font-size: 2vw;
 	}
 	.name {
 		display: flex;
 		flex-direction: column;
-		font-size: 14rem;
+		font-size: 10rem;
 		line-height: .8;
 	}
 	.divider {
