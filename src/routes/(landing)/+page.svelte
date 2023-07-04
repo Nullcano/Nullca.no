ac9af506b8fe6f97ae6fd0cfb012bd6fc17ca10e
Steffen Wold <woldsteffen@gmail.com>
2023-07-04 14:32:42 +0200

    update landing

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -4,21 +4,14 @@
   import Logo from '$lib/components/Logo.svelte'
 
   let interval
-  let images = ['/images/s-1.webp', '/images/s-2.webp', '/images/s-3.webp', '/images/s-4.webp'];
-  let currentIndex = 0;
+  let illustration = `"data:image/svg+xml,%3Csvg viewBox='0 0 320 180' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M114.5 84L105.5 73.5L95.5 81.6042M114.5 84L99 99L67 136.5H249L235 121M114.5 84L133 66L148 47L153 54L161 51L169 59L172 48L205 88L217 101M95.5 81.6042L104 94M95.5 81.6042L92 76.5L87 72.5L77.5 58.5L57 86L44.5 98M44.5 98L15 136.5M44.5 98L40.5 96L34 87.5L25.5 99.5L21 101.5L-0.50001 128M-9.77516e-06 165.75H320M-9.77516e-06 138.75H320M-9.77516e-06 141.75H320M-9.77516e-06 146.75H320M-9.77516e-06 154.75H320M-9.77516e-06 179.5H320M-9.77516e-06 136.5H320M137 179.5L144 136.5M91 179.5L112 136.5M46 179.5L76 136.5M-4 179.5L39 136.5M183 179.5L176 136.5M229 179.5L208 136.5M274 179.5L244 136.5M324 179.5L281 136.5M172 64.5L182.5 94.5L197 136.5M98 136.5L144.5 59L149 52M125.5 136.5L146 80.5L153.5 59M224 136.5L191 80.5L179 60M225 110L217 101M225 110L235 121M225 110L235 97.5L246 107M217 101L228 89.5L241 78.5L255.786 95.2571M235 121L246 107M255.786 95.2571L256 95.5M255.786 95.2571L246 107M255.786 95.2571L256 95L267 84L301 117L320 131M154 40L157 37.0236H163L166 40H154ZM153 26.5L156 29.5L156.035 36L153 39V26.5ZM167 39L164 36V29.5L167 26.5V39ZM154 12H166L163 15H157L154 12ZM153 25.5039V13L156.035 16.0236L156 22.5L153 25.5039ZM167 13V25.5039L164.047 22.5L164 16L167 13Z' stroke='white' stroke-opacity='0.1' stroke-width='0.5' fill='none' /%3E%3C/svg%3E"`
 
   let title = 'Nullcano'
   let title2 = 'AKA Steffen IRL'
   let animatedTitle = ''
   let animatedTitle2 = ''
 
-  function updateImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-  }
-
   onMount(() => {
-    interval = setInterval(updateImage, 150);
-
     const animateTitle = () => {
       animatedTitle = '';
       let index = 0;
@@ -58,107 +51,33 @@
   <title>{config.title}</title>
 </svelte:head>
 
-<section>
-  <figure>
-    <img src={`${images[currentIndex]}`} alt="Skeleton">
-  </figure>
-  <div class="content">
-    <div class="logo-provider">
-      <Logo />
-    </div>
-    <div class="title" data-title="{animatedTitle}">
-      {#if animatedTitle}
-        {animatedTitle}
-        {:else}
-        &nbsp;
-      {/if}
-    </div>
-    <div class="subtitle" data-title="{animatedTitle2}">
-      {#if animatedTitle2}
-        {@html animatedTitle2}
-        {:else}
-        &nbsp;
-      {/if}
-    </div>
-    <a href="/feed">
-      Please, after you...
-    </a>  
+<section class="br4 vw-100 vh-100 cover" style="background:url({illustration}) center">
+  <div class="pa5-l w-100 measure-l h-100 bg-black-20 bd-blur">
+    <div class="content w-100 h-100 flex flex-column space-between">
+      <div class="ma5 h3 self-start">
+        <Logo />
+      </div>
+      <div class="f2 mt5 mh5 self-start" data-title="{animatedTitle}">
+        {#if animatedTitle}
+          {animatedTitle}
+          {:else}
+          &nbsp;
+        {/if}
+      </div>
+      <div class="f4 mb5 mh5 self-start" data-title="{animatedTitle2}">
+        {#if animatedTitle2}
+          {@html animatedTitle2}
+          {:else}
+          &nbsp;
+        {/if}
+      </div>
+      <a class="ma5 pa3 ttu ba b--white-10 grow self-start" href="/feed">
+        Please, after you
+      </a>  
+    </div>  
   </div>
 </section>
 
 <style>
-  section {
-    position: relative;
-    margin: 3rem auto;
-    padding: 3rem;
-    max-width: 60em;
-    display: grid;
-    grid-template-columns: auto 1fr;
-    align-items: center;
-    justify-content: center;
-    gap: 1.5rem;
-    border: 1px solid hsl(0, 0%, 20%);
-  }
-  .logo-provider {
-    position: relative;
-    width: 6rem;
-  }
-  figure {
-    position: relative;
-    width: 20rem;
-    height: 0;
-    padding-bottom: 150%;
-    mix-blend-mode: exclusion;
-  }
-  figure img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    opacity: .25;
-    z-index: -1;
-  }
-  .content {
-    display: flex;
-    flex-direction: column;
-    gap: 1.5rem;
-  }
-  .title {
-    position: relative;
-    font-size: 3rem;
-    color: hsl(0, 0%, 80%);
-  }
-  .subtitle {
-    position: relative;
-    font-size: 1.5rem;
-    color: hsl(0, 0%, 80%);
-  }
-  a {
-    margin-top: 6rem;
-    position: relative;
-    border: 1px solid hsl(0, 0%, 20%);
-    padding: 1rem 2rem;
-    align-self: self-start;
-    font-weight: 600;
-    text-align: center;
-    background: hsl(0, 0%, 80%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    transition: all .25s linear;
-  }
-  a:hover {
-    transform: scale(1.1);
-  }
-  @media screen and (max-width:60em) {
-    .title {
-      font-size: 3rem;
-    }
-    .subtitle {
-      font-size: 1.5rem;
-    }
-    a {
-      font-size: 1rem;
-    }
-  }
+
 </style>
